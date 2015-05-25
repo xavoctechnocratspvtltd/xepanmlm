206eecd8b074f31ff571f261d86b2d87ea545e76
xavocvijay <guddavijay@gmail.com>
2015-05-25 18:12:54 +0530

    Repoart Page Added On xMLM Credits

---
+++ page/owner/xmlm/credits/report.php
@@ -0,0 +1,84 @@
+<?php
+
+class page_xMLM_page_owner_xmlm_credits_report extends page_xMLM_page_owner_main{
+	
+	function init(){
+		parent::init();
+
+		$distributer=$this->add('xMLM/Model_Distributor');
+		$form=$this->add('Form');
+		$form->addField('DatePicker','from_date');
+		$form->addField('DatePicker','to_date');
+		$form->addField('autocomplete/Basic','distributor')->setModel($distributer);
+		$form->addSubmit('Get Report');
+
+		$credit_mov=$this->add('xMLM/Model_CreditMovement');
+		$credit_mov->addExpression('credit')->set($credit_mov->dsql()->fx('IF',array($credit_mov->dsql()->expr('status="Purchase"'),$credit_mov->dsql()->expr('credits'),'0')));
+		$credit_mov->addExpression('debit')->set($credit_mov->dsql()->fx('IF',array($credit_mov->dsql()->expr('status="Consumed"'),$credit_mov->dsql()->expr('credits'),'0')));
+		
+
+		$grid=$this->add('Grid');
+
+			$this->api->stickyGET('distributor_id');
+			$this->api->stickyGET('from_date');
+			$this->api->stickyGET('to_date');
+
+			$q = $credit_mov->dsql();
+
+		
+			if($_GET['distributor_id']){
+				$credit_mov->addCondition('distributor_id',$_GET['distributor_id']);
+			}
+
+			if($_GET['from_date']){
+				$credit_mov->addCondition(
+						$q->orExpr()
+							->where(
+									$q->andExpr()
+										->where('status','Purchase')
+										->where('credits_given_on','>=',$_GET['from_date'])
+								)
+							->where(
+									$q->andExpr()
+										->where('status','Consumed')
+										->where('created_at','>=',$_GET['from_date'])
+								)
+					);
+			}
+
+			if($_GET['to_date']){
+				$credit_mov->addCondition(
+						$q->orExpr()
+							->where(
+									$q->andExpr()
+										->where('status','Purchase')
+										->where('credits_given_on','<',$this->api->nextDate($_GET['to_date']))
+								)
+							->where(
+									$q->andExpr()
+										->where('status','Consumed')
+										->where('created_at','<',$this->api->nextDate($_GET['to_date']))
+								)
+					);
+			}
+
+
+		$grid->setModel($credit_mov->debug());
+
+		$grid->removeColumn('item_name');
+		$grid->removeColumn('created_by');
+		$grid->removeColumn('related_document');
+
+		$grid->addQuickSearch(array('status','credits','credits_given_on'));
+
+		if($form->isSubmitted()){
+			$grid->js()->reload(
+						array(
+							'distributor_id'=>$form['distributor'],
+							'from_date'=>($form['from_date'])?:0,
+							'to_date'=>($form['to_date'])?:0,
+							)
+						)->execute();
+		}
+	}
+}
