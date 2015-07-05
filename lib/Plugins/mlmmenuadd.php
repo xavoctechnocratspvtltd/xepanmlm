3585ee7e53029b52aa641c0c7c89825c364c6bbd
sinharakesh <rksinha.btech@gmail.com>
2015-07-05 17:10:45 +0530

    freelook menu add

--- lib/Plugins/mlmmenuadd.php
+++ lib/Plugins/mlmmenuadd.php
@@ -26,6 +26,7 @@ class Plugins_mlmmenuadd extends \componentBase\Plugin {
                 $admin_m->addItem(array('Forms Management','icon'=>'gauge-1'),'xMLM_page_owner_forms');
                 $admin_m->addItem(array('Repurchase Management','icon'=>'gauge-1'),'xMLM_page_owner_repurchase');
                 $admin_m->addItem(array('Configuration','icon'=>'gauge-1'),'xMLM_page_owner_configuration');
+                $admin_m->addItem(array('Update FreeLook','icon'=>'gauge-1'),'xMLM_page_cron_freelookfinish');
         
 	}
 }
