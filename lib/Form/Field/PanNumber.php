6e110ccb2173edfef7e72b8bca9509ebf26c8151
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2015-07-05 20:28:08 +0530

    Avoide dupliate pannumbers

--- lib/Form/Field/PanNumber.php
+++ lib/Form/Field/PanNumber.php
@@ -10,7 +10,25 @@ class Form_Field_PanNumber extends \Form_Field_Line {
             if(strtolower($this->value[4]) != strtolower($last_name[0]) OR (strlen($this->value) !=10)){
                 $this->displayFieldError('Pan No Does not looks correct ');
             }
+
+            $model = $this->owner->model;
+
+            $check=  $this->add('xMLM/Model_Distributor');
+            $check->addCondition('pan_no',$this->value);
+
+            if($model->loaded()){
+                $check->addCondition('id','<>',$model->id);
+            }
+
+            $check->tryLoadAny();
+
+            if($check->loaded()){
+                $this->displayFieldError('Pan no is already used');
+            }
         }
+
+
+
         return parent::validate();
     }
 }
