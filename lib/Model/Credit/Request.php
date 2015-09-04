73eca9bda9aa22da7d3428562cd5323be957360b
Gowrav Vishwakarma <gowravvishwakarma@gmail.com>
2015-09-04 14:48:27 +0530

    Credit request amount edit facility in backend with beforesave Conditions

--- lib/Model/Credit/Request.php
+++ lib/Model/Credit/Request.php
@@ -9,11 +9,27 @@ class Model_Credit_Request extends Model_CreditMovement{
 			// 'can_mark_processed'=>array('caption'=>'Process'),
 			'can_reject'=>array(),
 			'allow_add'=>false,
+			'allow_edit'=>true,
 			'can_manage_attachments'=>false,
 		);
 
 	function init(){
 		parent::init();
 		$this->addCondition('status','Request');
+		$this->addHook('beforeSave',$this);
+	}
+	function beforeSave(){
+		$request_model=$this->add('xMLM/Model_Credit_Request');
+		$request_model->addCondition('id',$this['id']);
+		$request_model->tryLoadAny();
+		if($request_model->loaded()){
+			// throw new \Exception("loaded", 1);
+			if($request_model['credits'] < $this['credits'])
+				throw new \Exception("In-Sufficient Credits Amount ");
+		}
+		// throw new \Exception("not loaded", 1);
+		
+		
+		
 	}
 }
